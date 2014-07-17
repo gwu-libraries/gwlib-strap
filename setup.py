f5e84dc7dc69337d88a2f260944807df275eb0ee
Ankushi Sharma <anksharm@gwmail.gwu.edu>
2014-07-17 14:30:26 -0400

    first commit with all files and setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name='gwlib-strap',
+    version='0.1.0',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    package_data={
+        'gwlib-strap': ['gwlib-resources/img/*.png', '/gwlib-resources/img/*.gif', 'gwlib-resources/css/*.css'],
+        },
+    author='gwlibraries')
+
